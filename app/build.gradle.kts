52361cd1b7c6ae9dd233dd24828b50a73ea4f723
Prince Raj <Prince.raj.ds@gmail.com>
2026-07-08 04:23:42 +0530

    Bump version to 3.4 for clean release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,8 +18,8 @@ android {
     applicationId = "com.example.verse"
     minSdk = 24
     targetSdk = 36
-    versionCode = 9
-    versionName = "3.3"
+    versionCode = 10
+    versionName = "3.4"
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
   }
